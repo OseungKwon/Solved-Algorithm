5937a2937723fe7cd1fa4793e66fcda33cb8c409
OseungKwon <00osos@naver.com>
2021-07-28 09:53:14 +0900

    Create 더 맵게.py

---
+++ Programmers/Heap/더 맵게.py
@@ -0,0 +1,12 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville);
+    while K>scoville[0]:
+        if len(scoville)>1:
+            heapq.heappush(scoville, heapq.heappop(scoville)+heapq.heappop(scoville)*2)
+            answer+=1
+        else:
+            return -1
+
+    return answer
