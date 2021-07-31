e4941898c017c6ea0370ee9986af5b7d748a6a9d
OseungKwon <00osos@naver.com>
2021-08-01 02:37:09 +0900

    프로그래머스 문제풀이
    정렬> 가장 큰 수,
    완전 탐색> 카펫

--- Programmers/BruteForce/카펫.js
+++ Programmers/BruteForce/카펫.js
@@ -15,9 +15,24 @@ function solution(brown, yellow) {
 }
 */
 // 2트
+/*
 function solution(brown, yellow) {
     let yellowSqrt = Math.floor(Math.sqrt(yellow));
     for (let i = 1; i <= yellowSqrt; i++) {
+        for (let j = yellow; j >= 1; j--) {
+            if (i * j === yellow) {
+                if (brown === (i * 2 + j * 2 + 4)) {
+                    return [j + 2, i + 2]
+                }
+            }
+        }
+    }
+}
+*/
+// 3트
+// 굳이 yellowSqrt 안해도 return으로 함수 나감으로 그냥 yellow를 써주었다. 
+function solution(brown, yellow) {
+    for (let i = 1; i < yellow; i++) {
         for (let j = yellow; j >= 1; j--) {
             if (i * j === yellow) {
                 if (brown === (i * 2 + j * 2 + 4)) {
---
+++ Programmers/Sort/가장 큰 수.js
@@ -0,0 +1,37 @@
+/* 1트 테스트 케이스는 통과했지만 제출 시에 시간이 너무 소요되서 통과 못함
+function solution(numbers) {
+    var answer = '';
+    // 순열
+    const permutation = (numbers, range) => {
+        let output = []
+        if (range === 1) return numbers.map(num => [num]);
+
+        numbers.forEach((number, index, temp) => {
+            const mainNumber = number;
+            const subNumbers = numbers.filter((_, idx) => idx !== index);
+            const rePermutation = permutation(subNumbers, range - 1);
+            const combine = rePermutation.map(num => [mainNumber, ...num]);
+            output.push(...combine)
+        })
+        return output;
+    }
+
+    var arr = permutation(numbers, numbers.length)
+    var k = []
+    for (var i = 0; i < arr.length; i++) {
+        k.push(Number(arr[i].join('')))
+    }
+    answer = k.reduce((acc, cur) => acc > cur ? acc : cur)
+    return String(answer);
+}
+*/
+// 2트
+// 문자열 비교 시에 '10'>'1' 이 true값이 나오는 문제가 있었는데, 
+// 이는 sort시에 각 요소를 더한 값을 비교하므로써 해결했다.
+function solution(numbers) {
+    let answer = '';
+    const numbersToString = numbers.map(num => String(num))
+    numbersToString.sort((a, b) => (b + a) - (a + b))
+    answer = numbersToString.join('')
+    return answer[0] === '0' ? '0' : answer;
+}
