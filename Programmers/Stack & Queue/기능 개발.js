214e7e111ae5d1d0037c6f5363408be4ec65ec31
OseungKwon <00osos@naver.com>
2021-08-12 03:43:26 +0900

    Update 기능 개발.js
    다시 풀어보았는데, 전의 풀이와 약간의 차이점이 있었다.

--- Programmers/Stack & Queue/기능 개발.js
+++ Programmers/Stack & Queue/기능 개발.js
@@ -1,3 +1,4 @@
+/*
 function solution(progresses, speeds) {
     var answer = [];
     while (progresses.length > 0) {
@@ -10,6 +11,41 @@ function solution(progresses, speeds) {
             index++;
         }
         index > 0 && answer.push(index)
+    }
+    return answer;
+}
+*/
+// 시간 지나고 나서 다시 2트
+/*
+function solution(progresses, speeds) {
+    let answer = [];
+    while (progresses.length > 0) {
+        let distribute = 0;
+        while (progresses[0] < 100) {
+            progresses = progresses.map((progress, index) => progress += speeds[index])
+        }
+        while (progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            distribute++;
+        }
+        answer.push(distribute)
+    }
+    return answer;
+}
+*/
+// 3트
+function solution(progresses, speeds) {
+    let answer = [];
+    while (progresses.length > 0) {
+        let distribute = 0;
+        progresses = progresses.map((progress, index) => progress += speeds[index])
+        while (progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            distribute++;
+        }
+        distribute !== 0 && answer.push(distribute)
     }
     return answer;
 }
