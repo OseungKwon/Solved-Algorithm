016172f95d5ab4753c22617d410c0729b456050d
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-09-08 18:53:19 +0900

    Update 조이스틱.js

--- Baekjoon/탐욕 알고리즘/조이스틱.js
+++ Baekjoon/탐욕 알고리즘/조이스틱.js
@@ -5,43 +5,31 @@ function solution(name) {
     // 알파벳 탐색
     function search(word) {
         for (let i = 0; i < alphabet.length; i++) {
-            if (alphabet[i] === word || alphabet[alphabet.length - i] === word) {
-                console.log('find', answer)
+            if (alphabet[i] === word) {
+                console.log('findf', answer, i, word)
                 return answer;
+            } else if (alphabet[alphabet.length - 1 - i] === word) {
+                console.log('findb', answer, i, word)
+                return answer++;
             }
             answer++;
         }
     }
-    var findA = 0;
-    if (name[0] === 'A') {
-        for (let i = 0; i < name.length; i++) {
-            if (name[i] !== 'A') break;
-            findA++;
-        }
-    } else {
-        for (let i = 1; i < name.length; i++) {
-            if (name[i] !== 'A') break;
-            findA++;
+    for (let i = 0; i < name.length; i++) {
+        search(name[i]);
+    }
+    console.log(answer)
+    let minMove = name.length - 1;
+    for (let i = 1; i < name.length; i++) {
+        if (name[i] === 'A') {
+            for (var j = i + 1; j < name.length; j++) {
+                if (name[j] !== 'A') break;
+            }
+            const left = i - 1;
+            const right = name.length - j
+            minMove = Math.min(minMove, left > right ? left + right * 2 : left * 2 + right);
+            i = j;
         }
     }
-
-    console.log('a', findA)
-    if (findA === 0) {
-        for (let i = 0; i < name.length; i++) {
-            search(name[i])
-            if (i !== 0) answer++;
-        }
-    } else if (findA === name.length) {
-        return 0;
-
-    } else {
-        search(name[0])
-        for (let i = name.length - 1; i > findA; i--) {
-            search(name[i])
-            answer++
-        }
-    }
-
-
-    return answer;
+    return answer + minMove;
 }
