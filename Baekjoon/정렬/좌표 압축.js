429e5576cb94f6a791b61239009b2781b3c49149
OseungKwon <00osos@naver.com>
2021-09-20 23:23:10 +0900

    Edit 좌표 압축

--- Baekjoon/정렬/좌표 압축.js
+++ Baekjoon/정렬/좌표 압축.js
@@ -1,11 +1,16 @@
-let arr = [2, 4, -10, 4, -9]
+let arr = []
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+const n = +stdin[0];
+stdin[1] = stdin[1].split(" ").map((v) => +v);
+arr = [...stdin[1]];
 for (let i = 0; i < arr.length; i++) {
     arr[i] = [arr[i], i]
 }
 
 let result = []
 arr.sort((a, b) => a[0] - b[0])
-console.log(...arr)
+//console.log(...arr)
 let index = 0;
 for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i][0] === arr[i + 1][0]) {
@@ -15,4 +20,7 @@ for (let i = 0; i < arr.length - 1; i++) {
     }
 }
 result.push([index++, arr[arr.length - 1][1]])
-console.log(...result.sort((a, b) => a[1] - b[1]))
+result.sort((a, b) => a[1] - b[1])
+let k = []
+result.map(item => k.push(item[0]))
+console.log(k.join(' '))
