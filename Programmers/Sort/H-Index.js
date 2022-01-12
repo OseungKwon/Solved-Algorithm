c230a32b45dc1b306d0bb684ed5dcb1d852cb7fe
OseungKwon <00osos@naver.com>
2022-01-13 04:46:59 +0900

    Update H-Index.js

--- Programmers/Sort/H-Index.js
+++ Programmers/Sort/H-Index.js
@@ -1,12 +1,11 @@
 function solution(citations) {
-    let answer = 0;
-    let count = 0;
-    citations = citations.sort((a, b) => a - b)
-    for (let i = citations.length - 1; i >= 0; i--) {
-        count++;
-        if (count <= citations[i]) {
-            answer = count
-        }
+  citations = sort((a, b) => a - b);
+  let count = 0;
+  for (let i = citations.length - 1; i >= 0; i--) {
+    count++;
+    if (count > citations[i]) {
+      return --count;
     }
-    return answer;
+  }
+  return count;
 }
